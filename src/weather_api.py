3539ff202efa34daaff557b300bd72a39724f6c8
Ldrex1 <damzealwini7@gmail.com>
2025-01-21 03:58:47 +0100

    client setup

--- src/weather_api.py
+++ src/weather_api.py
@@ -6,4 +6,11 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 class weatherApi:
+    def __init__(self):
+        self.api_key = os.getenv('OPENWEATHER_API_KEY')
+        self.base_url = 'http://api.openweathermap.org/data/2.5/weather'
+        self.bucket_name =  os.getenv('AWS_S3_BUCKET_NAME')
+        self.region = os.getenv('AWS_REGION','eu-west-1')
+        self.s3_client = boto3.client('s3', region_name=self.region)
+
     
